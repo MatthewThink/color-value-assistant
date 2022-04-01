18ac5fcb9d6e91b86ada1e6850f5844b97da4282
修心 <1151468007@qq.com>
2022-04-01 20:12:36 +0800

    fc commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// 程序启动页
+
+const { USER_LOGIN } = require('/utils/constant.js');
+
+App({
+    onLaunch: function () {
+       
+    },
+
+    /**
+     * 用户登录
+     */
+   globalData:{
+     userInfo:{}
+   }
+})
